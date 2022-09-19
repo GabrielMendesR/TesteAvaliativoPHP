ac7fe8e4c9e3a0bc80902bc39665981661e976de
Gabriel Mendes Rodrigues <mendes@edu.unifil.br>
2022-09-19 00:07:18 -0300

    1.0

---
+++ Teste_Avaliativo/app/Http/Controllers/PostController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\Comment;
+use App\Http\Requests\StorePostRequest;
+use App\Http\Requests\UpdatePostRequest;
+
+class PostController extends Controller
+{
+    /*
+        Mostrar Todos os Itens
+    */
+    
+    public function index(Request $request)
+    {
+       
+    }
+
+  
+    public function store(Request $request)
+    {
+        /*new Post;
+        post::find...
+
+        new Comment;
+        comment::find...*/
+        
+    }
+
+   
+    public function show(Request $request)
+    {
+       
+    }
+
+
+    public function edit(Request $request)
+    {
+      
+    }
+
+   
+    public function destroy(Request $request)
+    {
+        
+    }
+}
